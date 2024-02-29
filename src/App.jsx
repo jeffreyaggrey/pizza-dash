b72d36358b94506930a189c8a71162ff804421c4
Jeffrey-Paul Aggrey <jeffreyaggrey@cvpcorp.com>
2024-02-28 21:58:27 -0600

    skaffold pizza-dash app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import Order from './features/order/Order';
+import AppLayout from './ui/AppLayout';
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/menu', element: <Menu /> },
+      { path: '/cart', element: <Cart /> },
+      { path: '/order', element: <Order /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
